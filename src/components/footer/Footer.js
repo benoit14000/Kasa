faae1f6148a8c84bf06e3f4cbe2bb22c69338819
benoit14000 <112859826+benoit14000@users.noreply.github.com>
2023-04-04 15:25:15 +0200

    Add files via upload

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
 	return (
 		<footer className='footer'>
 			<img src={logo} alt="Kasa - agence de location d'appartements entre particuliers" />
-			<p className='footer_copyright'>© 2020 Kasa. All rights reserved</p>
+			<p className='footer_copyright'>© 2023 Kasa. All rights reserved</p>
 		</footer>
 	)
 }
